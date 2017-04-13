1cf70362568dc63f0d2f7835f8a46fd2e2bfb40a
wegamekinglc <scrappedprince.li@gmail.com>
2017-04-13 14:26:24 +0800

    added file for remote windows call testing

---
+++ PySpyder/dags/remote_windows_call.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2016-4-13
+
+@author: cheng.li
+"""
+
+import datetime as dt
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+
+
+start_date = dt.datetime(2017, 1, 1)
+dag_name = 'remote_windows_call'
+
+default_args = {
+    'owner': 'wegamekinglc',
+    'depends_on_past': True,
+    'start_date': start_date
+}
+
+
+dag = DAG(
+    dag_id=dag_name,
+    default_args=default_args,
+    schedule_interval='*/5 * * * *')
+
+task = BashOperator(bash_command='ssh username@hostname "./remote_windows_test.bat"',
+                    dag=dag)
