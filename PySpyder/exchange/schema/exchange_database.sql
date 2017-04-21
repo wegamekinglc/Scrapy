5c3342501440f02965e36d3b7dd6075f60a3f003
wegamekinglc <scrappedprince.li@gmail.com>
2017-04-22 00:45:38 +0800

    update schema

--- PySpyder/exchange/schema/exchange_database.sql
+++ PySpyder/exchange/schema/exchange_database.sql
@@ -17,7 +17,15 @@ create table exchange.announcement_info
 	reportDate datetime not null,
 	instrumentID char(6) not null,
 	title text not null,
-	url text null
+	url text null,
+	updateTime datetime not null,
+	exchangePlace char(4) not null
 )
 ;
+
+create index announcement_info_reportDate_exchangePlace_index
+	on announcement_info (reportDate, exchangePlace)
+;
+
+
 
