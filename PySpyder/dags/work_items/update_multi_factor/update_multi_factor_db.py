34428bd3efd2a4886d54f8f0d349ba4e39f5282e
wegamekinglc <scrappedprince.li@gmail.com>
2017-05-10 09:31:53 +0800

    update update multi-factor script

--- PySpyder/dags/work_items/update_multi_factor/update_multi_factor_db.py
+++ PySpyder/dags/work_items/update_multi_factor/update_multi_factor_db.py
@@ -32,24 +32,29 @@ dag = DAG(
     schedule_interval='0 18 * * 1,2,3,4,5'
 )
 
+date_formatted_tables = {'FactorIndicator_500'}
+
 
 def create_ms_engine(db):
-    ms_user = ''
-    ms_pwd = ''
+    ms_user = 'sa'
+    ms_pwd = 'A12345678!'
     return sqlalchemy.create_engine(
         'mssql+pymssql://{0}:{1}@10.63.6.219/{2}?charset=cp936'.format(ms_user, ms_pwd, db))
 
 
 def create_my_engine():
-    my_user = ''
-    my_pwd = ''
+    my_user = 'sa'
+    my_pwd = 'We051253524522'
     return sqlalchemy.create_engine(
         'mysql+pymysql://{0}:{1}@rm-bp1psdz5615icqc0yo.mysql.rds.aliyuncs.com/multifactor?charset=utf8'.format(my_user, my_pwd))
 
 
 def fetch_date(table, query_date, engine):
     query_date = query_date.replace('-', '')
-    sql = "select * from {0} where Date = {1}".format(table, query_date)
+    if table in date_formatted_tables:
+        sql = "select * from {0} where Date = '{1}'".format(table, query_date)
+    else:
+        sql = "select * from {0} where Date = {1}".format(table, query_date)
     df = pd.read_sql_query(sql, engine)
 
     if table == 'FactorData':
@@ -58,7 +63,9 @@ def fetch_date(table, query_date, engine):
         cols[3] = '申万二级行业'
         cols[4] = '申万三级行业'
         df.columns = cols
-    df['Date'] = pd.to_datetime(df.Date.astype(str))
+
+    if table != 'FactorIndicator_500':
+        df['Date'] = pd.to_datetime(df.Date.astype(str))
     return df
 
 
@@ -166,6 +173,25 @@ def update_risk_factor_500(ds, **kwargs):
     return 0
 
 
+def update_factor_indicator(ds, **kwargs):
+    ref_date = kwargs['next_execution_date']\
+
+    if not isBizDay('china.sse', ref_date):
+        logger.info("{0} is not a business day".format(ref_date))
+        return 0
+
+    ref_date = ref_date.strftime('%Y-%m-%d')
+
+    conn1 = create_ms_engine('FactorPerformance')
+    df = fetch_date('FactorIndicator_500', ref_date, conn1)
+
+    conn2 = create_my_engine()
+
+    delete_data('factor_indicator', ref_date, conn2)
+    insert_data('factor_indicator', df, conn2)
+    return 0
+
+
 run_this1 = PythonOperator(
     task_id='update_factor_data',
     provide_context=True,
@@ -198,5 +224,12 @@ run_this5 = PythonOperator(
     task_id='update_risk_factor_500',
     provide_context=True,
     python_callable=update_risk_factor_500,
+    dag=dag
+)
+
+run_this6 = PythonOperator(
+    task_id='update_factor_indicator',
+    provide_context=True,
+    python_callable=update_factor_indicator,
     dag=dag
 )
