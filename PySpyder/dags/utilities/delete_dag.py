edaba6a2826360bb10356620d49f418e2472b8d1
wegamekinglc <scrappedprince.li@gmail.com>
2017-09-02 23:17:02 +0800

    fixed dialect

--- PySpyder/dags/utilities/delete_dag.py
+++ PySpyder/dags/utilities/delete_dag.py
@@ -6,34 +6,35 @@ Created on 2016-4-15
 """
 
 import sys
-import pymysql
+import psycopg2
 
-DB_SETTINGS = {'hostname': 'hostname',
-               'user': 'user',
-               'passwd': 'passwd',
-               'db': 'db'}
+DB_SETTINGS = {'hostname': '192.168.0.101',
+               'user': 'postgres',
+               'passwd': 'we083826',
+               'db': 'airflow'}
 
-QUERIES = {'delete from xcom where dag_id = "{dag_id}"',
-           'delete from task_fail where dag_id = "{dag_id}"',
-           'delete from task_instance where dag_id = "{dag_id}"',
-           'delete from sla_miss where dag_id = "{dag_id}"',
-           'delete from log where dag_id = "{dag_id}"',
-           'delete from job where dag_id = "{dag_id}"',
-           'delete from dag_run where dag_id = "{dag_id}"',
-           'delete from dag where dag_id = "{dag_id}"',
-           'delete from dag_stats where dag_id = "{dag_id}"',
-           'delete from task_fail where dag_id = "{dag_id}"',
-           'delete from task_instance where dag_id = "{dag_id}"'}
+QUERIES = {"delete from xcom where dag_id = '{dag_id}'",
+           "delete from task_fail where dag_id = '{dag_id}'",
+           "delete from task_instance where dag_id = '{dag_id}'",
+           "delete from sla_miss where dag_id = '{dag_id}'",
+           "delete from log where dag_id = '{dag_id}'",
+           "delete from job where dag_id = '{dag_id}'",
+           "delete from dag_run where dag_id = '{dag_id}'",
+           "delete from dag where dag_id = '{dag_id}'",
+           "delete from dag_stats where dag_id = '{dag_id}'",
+           "delete from task_fail where dag_id = '{dag_id}'",
+           "delete from task_instance where dag_id = '{dag_id}'"}
 
 
 def delete_dag(dag_id):
     """
     Delete specific dag from the airflow db
     """
-    with pymysql.connect(host=DB_SETTINGS['hostname'],
-                         user=DB_SETTINGS['user'],
-                         passwd=DB_SETTINGS['passwd'],
-                         db=DB_SETTINGS['db']) as cur:
+    with psycopg2.connect(host=DB_SETTINGS['hostname'],
+                          user=DB_SETTINGS['user'],
+                          password=DB_SETTINGS['passwd'],
+                          database=DB_SETTINGS['db']) as conn:
+        cur = conn.cursor()
         for query in QUERIES:
             print(query.format(dag_id=dag_id))
             cur.execute(query.format(dag_id=dag_id))
